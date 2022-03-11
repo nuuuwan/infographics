5bbdf7c938064e9334be5de89e13f66dff2542c2
nuuuwan <nuuuwan@gmail.com>
2022-03-11 19:34:45 +0530

    Shell LKMap

--- src/infographics/geo/LKMap.py
+++ src/infographics/geo/LKMap.py
@@ -1,2 +1,18 @@
+from geo import geodata
+from utils import ds
+from infographics.core import SVGPalette
+
+
 class LKMap():
-    pass
+    @property
+    def xml(self):
+        palette = SVGPalette()
+        df = geodata.get_region_geodata(
+            'LK',
+            'district',
+        )
+        latlng_list_list_list = []
+        for row in df.itertuples():
+            multi_polygon_list.append(row.geometry)
+
+        return palette.draw_text('TODO')
---
+++ src/infographics/geo/__init__.py
@@ -0,0 +1,1 @@
+from infographics.geo.LKMap import LKMap
