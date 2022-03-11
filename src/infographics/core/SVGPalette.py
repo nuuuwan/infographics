7739251b71fc2fec5b16e6077318d5c5fc7bc27b
nuuuwan <nuuuwan@gmail.com>
2022-03-11 20:17:13 +0530

    Added draw_p_list functions

--- src/infographics/core/SVGPalette.py
+++ src/infographics/core/SVGPalette.py
@@ -58,3 +58,25 @@ class SVGPalette(SVGPaletteSize):
             SVG_STYLES.SVG | {
                 'width': self.width,
                 'height': self.height})
+
+    def draw_p_list_list_list(self, p_list_list_list):
+        return _('g', list(map(
+            self.draw_p_list_list,
+            p_list_list_list,
+        )))
+
+    def draw_p_list_list(self, p_list_list):
+        return _('g', list(map(
+            self.draw_p_list,
+            p_list_list,
+        )))
+
+    def draw_p_list(self, p_list):
+        d_list = []
+        for p in p_list:
+            x, y = self.t(p)
+            prefix = 'M' if (not d_list) else 'L'
+            d_list.append('%s%d,%d' % (prefix, x, y))
+        d_list.append('z')
+        d = ' '.join(d_list)
+        return _('path', None, SVG_STYLES.PATH | {'d': d})
