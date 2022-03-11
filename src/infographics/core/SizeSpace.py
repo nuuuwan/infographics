b9247d26477939a477e17d27b5ba2a67ed806cca
nuuuwan <nuuuwan@gmail.com>
2022-03-11 18:37:54 +0530

    SizeSpace

---
+++ src/infographics/core/SizeSpace.py
@@ -0,0 +1,31 @@
+from abc import ABC
+
+
+class SizeSpace(ABC):
+    @property
+    def width(self):
+        return self.size[0]
+
+    @property
+    def height(self):
+        return self.size[1]
+
+    @property
+    def padding(self):
+        return self.size[2]
+
+    @property
+    def actual_width(self):
+        return self.width - self.padding * 2
+
+    @property
+    def actual_height(self):
+        return self.height - self.padding * 2
+
+    def t(self, p):
+        px, py = p
+        qx, qy = (px + 1) / 2, (py + 1) / 2
+        x = self.padding + qx * self.actual_width
+        y = self.padding + (1 - qy) * self.actual_height
+        print(p, (x, y))
+        return (x, y)
