7a6b54ade49d7be2504e3834a0b521c6d82ff6ec
nuuuwan <nuuuwan@gmail.com>
2022-03-11 18:38:04 +0530

    SVG_STYLES

---
+++ src/infographics/core/SVG_STYLES.py
@@ -0,0 +1,27 @@
+DEFAULT_FONT = 'Gill Sans'
+
+
+class SVG_STYLES:
+    SVG = {
+        'xmlns': 'http://www.w3.org/2000/svg',
+
+    }
+
+    TEXT = {
+        'fill': 'black',
+        'stroke': 'none',
+        'text-anchor': 'middle',
+        'dominant-baseline': 'middle',
+        'font-family': DEFAULT_FONT,
+    }
+
+    RECT = {
+        'fill': 'white',
+        'stroke': 'lightgray',
+        'stroke-width': 1,
+    }
+
+    LINE = {
+        'stroke': 'lightgray',
+        'stroke-width': 1,
+    }
