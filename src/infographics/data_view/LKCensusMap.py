471a739061671805c941801c846478fc0e8c3f60
nuuuwan <nuuuwan@gmail.com>
2022-03-12 22:38:20 +0530

    + LKCensusMap

---
+++ src/infographics/data_view/LKCensusMap.py
@@ -0,0 +1,36 @@
+from infographics.core import ColorPaletteVaryLightness
+from infographics.data import LKCensusData
+from infographics.data_view.LKMap import LKMap
+
+
+class LKCensusMap(LKCensusData, LKMap):
+    DEFAULT_FIELD = 'sinhalese'
+
+    def __init__(
+        self,
+        table_id=LKCensusData.DEFAULT_TABLE_ID,
+        field=DEFAULT_FIELD,
+        region_id=LKMap.DEFAULT_REGION_ID,
+        subregion_type=LKMap.DEFAULT_SUBREGION_TYPE,
+    ):
+        LKCensusData.__init__(self, table_id)
+        LKMap.__init__(
+            self,
+            region_id,
+            subregion_type,
+            legend_title='% of Population',
+            color_palette=ColorPaletteVaryLightness(),
+        )
+        self.field = field
+
+    # Implement AbstractColoredView
+    def get_color_value(self, id):
+        d = self.lk_census_data[id]
+        return d[self.field] / d[self.total_field]
+
+    # Implement AbstractLabelledView
+    def get_label(self, id):
+        return self.region_id
+
+    def get_label_value(self, id):
+        return self.lk_census_data[id][self.field]
--- src/infographics/data_view/__init__.py
+++ src/infographics/data_view/__init__.py
@@ -1,1 +1,2 @@
+from infographics.data_view.LKCensusMap import LKCensusMap
 from infographics.data_view.LKMap import LKMap
