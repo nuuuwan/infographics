10fe5a03120be2526973f758686d682990b3a9a6
nuuuwan <nuuuwan@gmail.com>
2022-03-12 16:44:29 +0530

    Remove geodata_index set

--- src/infographics/data/LKGeoData.py
+++ src/infographics/data/LKGeoData.py
@@ -7,7 +7,6 @@ class LKGeoData:
     def __init__(self, region_id, subregion_type):
         self.region_id = region_id
         self.subregion_type = subregion_type
-        self.geodata_index = self.get_geodata_index()
 
     def get_geodata_index(self):
         df = geodata.get_region_geodata(
