62060d7a241661d132059faca9adcd3d8d3c648d
nuuuwan <nuuuwan@gmail.com>
2022-03-12 10:21:11 +0530

    Fixed child_list #BUG

--- src/infographics/core/SVGPalettePolygon.py
+++ src/infographics/core/SVGPalettePolygon.py
@@ -9,20 +9,26 @@ DEFAULT_BASE_FONT_SIZE = 16
 
 class SVGPalettePolygon:
     def draw_multi2polygon(self, multi2polygon, child_list=[], attribs={}):
-        return self.draw_g(list(map(
-            lambda multipolygon: self.draw_multipolygon(
-                multipolygon,
-                child_list,
-                attribs,
-            ),
-            multi2polygon,
-        )), attribs)
+        return self.draw_g(
+            list(map(
+                lambda multipolygon: self.draw_multipolygon(
+                    multipolygon,
+                    [],
+                    attribs,
+                ),
+                multi2polygon,
+            )) + child_list,
+            attribs,
+        )
 
     def draw_multipolygon(self, multipolygon, child_list=[], attribs={}):
-        return self.draw_g(list(map(
-            lambda polygon: self.draw_polygon(polygon, child_list, attribs),
-            multipolygon,
-        )), attribs)
+        return self.draw_g(
+            list(map(
+                lambda polygon: self.draw_polygon(polygon, [], attribs),
+                multipolygon,
+            )) + child_list,
+            attribs,
+        )
 
     def draw_polygon(self, polygon, child_list, attribs={}):
         d_list = []
