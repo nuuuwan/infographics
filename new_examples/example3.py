f3d3a9f0ea344ae5c13ac8f04cec3e9be222b657
nuuuwan <nuuuwan@gmail.com>
2022-03-12 23:00:58 +0530

    Updated custom params

--- new_examples/example3.py
+++ new_examples/example3.py
@@ -1,11 +1,18 @@
-from infographics.core import Infographic
+from infographics.core import ColorPaletteVaryLightness, Infographic
 from infographics.data_view import LKCensusMap
 
 Infographic(
-    title='Sinhalese Population',
+    title='Buddhist Population',
     subtitle='Provinces of Sri Lanka',
     footer_text='visualization by @nuuuwan',
-    child_list=[LKCensusMap(
-        subregion_type='district',
-    )]
-).save('/tmp/infographics.example3.svg')
+    child_list=[
+        LKCensusMap(
+            region_id='LK-11',
+            subregion_type='dsd',
+            table_id='religious_affiliation_of_population',
+            field='buddhist',
+            color_palette=ColorPaletteVaryLightness(
+                hue=60,
+                min_lightness=1,
+                max_lightness=0.4),
+        )]).save('/tmp/infographics.example3.svg')
