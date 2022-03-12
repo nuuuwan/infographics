9428fde9dd99ec851fd84a12b5103ba967355ad3
nuuuwan <nuuuwan@gmail.com>
2022-03-12 12:13:49 +0530

    Fixed format_population

--- src/infographics/view/format.py
+++ src/infographics/view/format.py
@@ -1,8 +1,23 @@
+
+
 def format_population(population):
     if population > 1_000_000:
-        population_m = population / 1_000_000
-        return f'{population_m:1.1f}M'
+        population_x = population / 1_000_000
+        return f'{population_x:1.1f}M'
+    if population > 100_000:
+        population_x = population / 10_000
+        return f'{population_x:2.0f}0K'
+
+    if population > 10_000:
+        population_x = population / 1_000
+        return f'{population_x:2.0f}K'
+
     if population > 1_000:
-        population_k = population / 1_000
-        return f'{population_k:2.0f}K'
-    return population
+        population_x = population / 1_000
+        return f'{population_x:1.1f}K'
+
+    if population > 1_00:
+        population_x = population / 10
+        return f'{population_x:2.0f}0'
+
+    return f'{population:2.0f}'
