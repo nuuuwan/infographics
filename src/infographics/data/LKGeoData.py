b72508fb0fc39aaac568e70351a71f46ffd404fa
nuuuwan <nuuuwan@gmail.com>
2022-03-12 10:22:25 +0530

    Use geodata_index

--- src/infographics/data/LKGeoData.py
+++ src/infographics/data/LKGeoData.py
@@ -1,6 +1,5 @@
 from geo import geodata
 
-from infographics.core import SVGPalette
 from infographics.math import latlng
 
 
@@ -8,13 +7,12 @@ class LKGeoData:
     def __init__(self, region_id, subregion_type):
         self.region_id = region_id
         self.subregion_type = subregion_type
+        self.geodata_index = self.get_geodata_index()
 
-    @property
-    def geodata_list(self):
-        SVGPalette()
+    def get_geodata_index(self):
         df = geodata.get_region_geodata(
             self.region_id,
             self.subregion_type,
         )
-        geodata_list = latlng.df_to_geodata_list(df)
-        return geodata_list
+        geodata_index = latlng.df_to_geodata_index(df)
+        return geodata_index
