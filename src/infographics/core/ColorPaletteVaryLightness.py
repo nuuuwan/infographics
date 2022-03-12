2de77e2f06cc6e40dd99bcc9f9787f4eab566fd9
nuuuwan <nuuuwan@gmail.com>
2022-03-12 13:44:26 +0530

    Added ColorPalettes

---
+++ src/infographics/core/ColorPaletteVaryLightness.py
@@ -0,0 +1,15 @@
+from utils import colorx
+
+
+class ColorPaletteVaryLightness:
+    def __init__(self, hue=0, min_lightness=0.8, max_lightness=0.2, n=1):
+        self.hue = hue
+        self.min_lightness = min_lightness
+        self.max_lightness = max_lightness
+        self.n = n
+
+    def color(self, p):
+        lightness_span = self.max_lightness - self.min_lightness
+        lightness = self.min_lightness + lightness_span * p / self.n
+        color = colorx.random_hsl(hue=self.hue, lightness=lightness)
+        return color
--- src/infographics/core/__init__.py
+++ src/infographics/core/__init__.py
@@ -1,2 +1,5 @@
+from infographics.core.ColorPaletteVaryHue import ColorPaletteVaryHue
+from infographics.core.ColorPaletteVaryLightness import \
+    ColorPaletteVaryLightness
 from infographics.core.Infographic import Infographic
 from infographics.core.SVGPalette import SVGPalette
