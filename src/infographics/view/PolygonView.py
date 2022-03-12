d8d612784dd89ec2ec48de408722074e0fa83899
nuuuwan <nuuuwan@gmail.com>
2022-03-12 09:20:38 +0530

    Added polygon colors

--- src/infographics/view/PolygonView.py
+++ src/infographics/view/PolygonView.py
@@ -3,10 +3,27 @@ from infographics.core import SVGPalette
 
 
 class PolygonView:
-    def __init__(self, multi2polygon=[]):
-        self.multi2polygon = multi2polygon
+    def __init__(self, multi2polygon=None, color_list=None):
+
+        self.multi2polygon = multi2polygon if multi2polygon else []
+        if color_list:
+            self.color_list = color_list
+        else:
+            self.color_list = SVGPalette.get_random_color_list(len(self))
+
+    def __len__(self):
+        return len(self.multi2polygon)
 
     @property
     def xml(self):
         palette = SVGPalette()
-        return palette.draw_multi2polygon(self.multi2polygon)
+
+        return palette.draw_g(
+            list(map(
+                lambda x: palette.draw_multipolygon(
+                    x[1],
+                    {'fill': self.color_list[x[0]]},
+                ),
+                enumerate(self.multi2polygon),
+            )),
+        )
