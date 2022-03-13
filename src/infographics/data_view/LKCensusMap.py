a485a212e6bdc5210fdc494fc51048c3ec658be3
nuuuwan <nuuuwan@gmail.com>
2022-03-13 10:36:42 +0530

    Updated LKCensusMap to use components

--- src/infographics/data_view/LKCensusMap.py
+++ src/infographics/data_view/LKCensusMap.py
@@ -1,38 +1,43 @@
-from infographics.core import ColorPaletteVaryLightness
 from infographics.data import LKCensusData
 from infographics.data_view.LKMap import LKMap
 
 
-class LKCensusMap(LKCensusData, LKMap):
-    DEFAULT_FIELD = 'sinhalese'
-    DEFAULT_COLOR_PALETTE = ColorPaletteVaryLightness(hue=0)
-
+class LKCensusMap:
     def __init__(
         self,
-        table_id=LKCensusData.DEFAULT_TABLE_ID,
-        field=DEFAULT_FIELD,
-        region_id=LKMap.DEFAULT_REGION_ID,
-        subregion_type=LKMap.DEFAULT_SUBREGION_TYPE,
-        color_palette=DEFAULT_COLOR_PALETTE,
+        # LKCensusData
+        table_id,
+
+        # LKMap
+        region_id,
+        subregion_type,
+        legend_title,
+        color_palette,
+
+        # LKCensusMap
+        field,
+
     ):
-        LKCensusData.__init__(self, table_id)
-        LKMap.__init__(
-            self,
+        self.data = LKCensusData(table_id)
+        self.view = LKMap(
             region_id,
             subregion_type,
-            legend_title='% of Population',
-            color_palette=color_palette,
+            legend_title,
+            color_palette,
         )
         self.field = field
 
+    def __xml__(self):
+        return self.view.__xml__()
+
     # Implement AbstractColoredView
     def get_color_value(self, id):
-        d = self.lk_census_data[id]
+        d = self.data[id]
         return d[self.field] / d[self.total_field]
 
     # For AbstractLabelledView
     def get_label(self, id):
-        return self.lk_geo_data[id]['name']
+        return self.view.data[id]['name']
 
     def get_label_value(self, id):
         return self.get_color_value(id)
