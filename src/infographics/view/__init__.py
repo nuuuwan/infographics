c4aa233ea59ec2dc2fd3423752de2689ac564a7b
nuuuwan <nuuuwan@gmail.com>
2022-03-13 11:40:49 +0530

    Added DorlingView

--- src/infographics/view/__init__.py
+++ src/infographics/view/__init__.py
@@ -1,3 +1,4 @@
 from infographics.view.ColoredView import ColoredView
+from infographics.view.DorlingView import DorlingView
 from infographics.view.LabelledView import LabelledView
 from infographics.view.PolygonView import PolygonView
