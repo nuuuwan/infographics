002800549a29323b17a903c9534e3136cefdc93a
nuuuwan <nuuuwan@gmail.com>
2022-03-17 19:46:33 +0530

    Multiple maps

--- new_examples/example6.py
+++ new_examples/example6.py
@@ -3,21 +3,39 @@ from infographics.data_view import LKCensusMap
 from infographics.view import FlagDorlingView
 
 
-def main():
+def sub(region_id, subregion_type):
+
+    svg_file_name = '/tmp/infographics.example6' + \
+        f'-{region_id}-{subregion_type}.svg'
+
     Infographic(
         title='SL Population',
-        subtitle='Flag Cartogram',
+        subtitle=f'Flag Cartogram by {subregion_type.title()}',
         footer_text='visualization by @nuuuwan',
         child_list=[
             LKCensusMap(
                 region_id='LK',
-                subregion_type='country',
+                subregion_type=subregion_type,
                 legend_title='',
                 table_id='ethnicity_religion',
                 field='all',
                 color_palette=None,
                 class_view=FlagDorlingView,
-            )]).save('/tmp/infographics.example6.svg')
+            )]).save(svg_file_name)
+
+
+def main():
+    for region_id, subregion_type in [
+        ['LK', 'country'],
+        ['LK', 'province'],
+        ['LK', 'district'],
+        ['LK', 'pd'],
+        ['LK-11', 'pd'],
+        ['LK-12', 'pd'],
+        ['LK-62', 'pd'],
+        ['LK-52', 'pd'],
+    ]:
+        sub(region_id, subregion_type)
 
 
 if __name__ == '__main__':
