571b8e24d44c65c2ff9d6992d91f5ae8c992c33d
nuuuwan <nuuuwan@gmail.com>
2022-03-18 15:08:31 +0530

    Updated various params

--- new_examples/example2.py
+++ new_examples/example2.py
@@ -1,16 +1,26 @@
+from gig import ent_types, ents
+
 from infographics.base import xy
 from infographics.core import Infographic
 from infographics.core.SVGPalette import SVGPalette
 from infographics.data import LKGeoData
-from infographics.view import PolygonView
+from infographics.view import LegendView, PolygonView
 from new_examples.examples import example_svg_file_name
 
 
 def main():
+    region_id = 'LK-1'
+    subregion_type = 'dsd'
+    region_ent = ents.get_entity(region_id)
+    region_name = region_ent['name']
+    region_entity_type = ent_types.get_entity_type(region_id)
+    title = f'{region_name} {region_entity_type.upper()}'
+    subtitle = f'Population Density by {subregion_type.upper()}'
+
     palette = SVGPalette()
     lk_geodata = LKGeoData(
-        region_id='LK',
-        subregion_type='district',
+        region_id=region_id,
+        subregion_type=subregion_type,
     ).data
     multi2polygon = list(map(lambda d: d['multipolygon'], lk_geodata.values()))
     norm_multi2polygon = xy.get_norm_multi2polygon(multi2polygon)
@@ -20,18 +30,25 @@ def main():
     def get_id_to_norm_multipolygon(id):
         return lk_geodata[id]['norm_multipolygon']
 
+    color_metadata = [
+        (2000, None, 'maroon'),
+        (1000, 2000, 'red'),
+        (500, 1000, 'orange'),
+        (250, 500, 'yellow'),
+        (125, 250, 'green'),
+        (None, 125, 'blue'),
+    ]
+
     def get_id_to_color(id):
         d = lk_geodata[id]
         density = d['population'] / d['area']
-        if density > 1000:
-            return 'red'
-        if density > 500:
-            return 'orange'
-        if density > 250:
-            return 'yellow'
-        if density > 125:
-            return 'green'
-        return 'blue'
+        for min_value, max_value, color in color_metadata:
+            if min_value and min_value > density:
+                continue
+            if max_value and max_value <= density:
+                continue
+            return color
+        return 'gray'
 
     def get_id_to_label(id, cxy, rxy):
         label = lk_geodata[id]['name']
@@ -43,9 +60,25 @@ def main():
             font_size,
         )
 
+    def get_color_value(x):
+        min_value, max_value, color = x
+        connector = ' - ' if (min_value and max_value) else ' '
+        min_value = min_value if min_value else '<'
+        max_value = max_value if max_value else '<'
+        return f'{min_value}{connector}{max_value}'
+
+    color_value_to_color = dict(list(map(
+        lambda x: (get_color_value(x), x[2]),
+        color_metadata,
+    )))
+    color_values = color_value_to_color.keys()
+
+    def get_color_value_to_color(color_value):
+        return color_value_to_color[color_value]
+
     infographic = Infographic(
-        title='Population & Population Density',
-        subtitle='Provinces of Sri Lanka',
+        title=title,
+        subtitle=subtitle,
         footer_text='visualization by @nuuuwan',
         children=[
             PolygonView(
@@ -54,6 +87,11 @@ def main():
                 get_id_to_color=get_id_to_color,
                 get_id_to_label=get_id_to_label,
                 children=[],
+            ),
+            LegendView(
+                legend_title='Persons per km²',
+                color_values=color_values,
+                get_color_value_to_color=get_color_value_to_color,
             )
         ]
     )
