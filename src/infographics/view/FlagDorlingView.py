80207504fbde4932e6bd172136e7b7e8a8ae5ec3
nuuuwan <nuuuwan@gmail.com>
2022-03-18 18:02:18 +0530

    Refactored

--- src/infographics/view/FlagDorlingView.py
+++ src/infographics/view/FlagDorlingView.py
@@ -1,17 +1,40 @@
 
-from infographics.view.ColoredView import KEY_TO_COLOR
 from infographics.view.DorlingView import DorlingView
 
 CIRLCE_RADIUS_MAX_LABEL_VALUE = 0.2
 
 
 class FlagDorlingView(DorlingView):
-    def render_dorling_object(self, id, xy, r, attribs):
-        label_data = self.get_label_data(id)
-        n_sinhalese = label_data['sinhalese']
-        n_tamil = (label_data['sl_tamil'] + label_data['ind_tamil'])
-        n_moor = (label_data['sl_moor'] + label_data['malay'])
-        n_buddhist = (label_data['buddhist'])
+    def __init__(
+        self,
+        ids,
+        get_id_to_norm_multipolygon,
+        get_id_to_label,
+        get_id_to_cartogram_value,
+        get_id_to_flag_data,
+        children=[],
+    ):
+        def get_id_to_color(id):
+            return 'gray'
+
+        DorlingView.__init__(
+            self,
+            ids=ids,
+            get_id_to_norm_multipolygon=get_id_to_norm_multipolygon,
+            get_id_to_color=get_id_to_color,
+            get_id_to_label=get_id_to_label,
+            get_id_to_cartogram_value=get_id_to_cartogram_value,
+            children=children,
+
+        )
+        self.get_id_to_flag_data = get_id_to_flag_data
+
+    def render_dorling_object(self, id, xy, r):
+        flag_data = self.get_id_to_flag_data(id)
+        n_sinhalese = flag_data['sinhalese']
+        n_tamil = flag_data['tamil']
+        n_moor = flag_data['muslim']
+        n_buddhist = flag_data['buddhist']
 
         total = n_sinhalese + n_tamil + n_moor
         p_sinhalese = n_sinhalese / total
@@ -21,17 +44,17 @@ class FlagDorlingView(DorlingView):
         p_buddhist = n_buddhist / n_sinhalese
 
         x, y = xy
-        rx = r
-        ry = rx * 9 / 16
+        rx = r * 0.8
+        ry = rx
 
         x0 = x - rx
         y0 = y + ry
         inner_list = []
         for [color, p] in [
-            [KEY_TO_COLOR['sl_moor'], p_moor],
-            [KEY_TO_COLOR['sl_tamil'], p_tamil],
-            [KEY_TO_COLOR['buddhist'], p_sinhalese * p_buddhist],
-            [KEY_TO_COLOR['sinhalese'], p_sinhalese * (1 - p_buddhist)],
+            ['green', p_moor],
+            ['orange', p_tamil],
+            ['yellow', p_sinhalese * p_buddhist],
+            ['maroon', p_sinhalese * (1 - p_buddhist)],
         ]:
             rx0 = rx * p
             inner_list.append(
