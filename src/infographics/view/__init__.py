733dc8bfe3a59f8082bb16b73a5563cbb7327928
nuuuwan <nuuuwan@gmail.com>
2022-03-18 15:08:56 +0530

    Added LegendView(2)

--- src/infographics/view/__init__.py
+++ src/infographics/view/__init__.py
@@ -2,4 +2,5 @@ from infographics.view.ColoredView import ColoredView
 from infographics.view.DorlingView import DorlingView
 from infographics.view.FlagDorlingView import FlagDorlingView
 from infographics.view.LabelledView import LabelledView
+from infographics.view.LegendView import LegendView
 from infographics.view.PolygonView import PolygonView
