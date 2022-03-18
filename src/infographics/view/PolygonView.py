cd4b8f896bf6aa604d4c6594e33e8951321b6be3
nuuuwan <nuuuwan@gmail.com>
2022-03-18 14:28:13 +0530

    multi_polygon to norm_multipolygon

--- src/infographics/view/PolygonView.py
+++ src/infographics/view/PolygonView.py
@@ -8,13 +8,13 @@ class PolygonView(ABC):
     def __init__(
         self,
         ids,
-        get_id_to_multipolygon,
+        get_id_to_norm_multipolygon,
         get_id_to_color,
         get_id_to_label,
         children=[],
     ):
         self.ids = ids
-        self.get_id_to_multipolygon = get_id_to_multipolygon
+        self.get_id_to_norm_multipolygon = get_id_to_norm_multipolygon
         self.get_id_to_color = get_id_to_color
         self.get_id_to_label = get_id_to_label
         self.children = children
@@ -23,19 +23,19 @@ class PolygonView(ABC):
     def __xml__(self):
         inner_child_list = []
         for id in self.ids:
-            multipolygon = self.get_id_to_multipolygon(id)
+            norm_multipolygon = self.get_id_to_norm_multipolygon(id)
 
             inner_child_list.append(
                 self.palette.draw_multipolygon(
-                    multipolygon,
+                    norm_multipolygon,
                     [],
                     {'fill': self.get_id_to_color(id)},
                 )
             )
 
         for id in self.ids:
-            multipolygon = self.get_id_to_multipolygon(id)
-            (cx, cy), (rx, ry) = xy.get_cxcyrxry(multipolygon)
+            norm_multipolygon = self.get_id_to_norm_multipolygon(id)
+            (cx, cy), (rx, ry) = xy.get_cxcyrxry(norm_multipolygon)
             inner_child_list.append(
                 self.get_id_to_label(id, (cx, cy), (rx, ry)),
             )
