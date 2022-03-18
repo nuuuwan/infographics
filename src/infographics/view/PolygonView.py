f2b37d1f183ea1056b7de83f92ed01a98d411da1
nuuuwan <nuuuwan@gmail.com>
2022-03-18 11:45:07 +0530

    Streamlined PolygonView

--- src/infographics/view/PolygonView.py
+++ src/infographics/view/PolygonView.py
@@ -12,51 +12,24 @@ class PolygonView(ABC):
 
     def __init__(
         self,
-        # For ColoredView
-        keys,
-        get_color_value,
-        legend_title,
-        color_palette,
-
-        # For LabelledView
-        get_label_data,
-
-        # For PolygonView
-        id_to_multipolygon,
-
-        # classes
-        class_colored_view=DEFAULT_CLASS_COLORED_VIEW,
-        class_labelled_view=DEFAULT_CLASS_LABELLED_VIEW,
+        ids,
+        get_id_to_multipolygon,
+        get_id_to_color,
+        get_id_to_label,
+        children=[],
     ):
-        self.colored_view = class_colored_view(
-            keys,
-            get_color_value,
-            legend_title,
-            color_palette,
-        )
-
-        self.get_label_data = get_label_data
-
-        self.labelled_view = class_labelled_view(
-            keys,
-            get_label_data,
-            self.get_label_xy,
-            self.get_label_r,
-        )
-        self.id_to_multipolygon = id_to_multipolygon
+        self.ids = ids
+        self.get_id_to_multipolygon = get_id_to_multipolygon
+        self.get_id_to_color = get_id_to_color
+        self.get_id_to_label = get_id_to_label
+        self.children = children
         self.palette = SVGPalette()
-
-    def __len__(self):
-        return len(self.id_to_multipolygon)
-
-    def get_multipolygon(self, id):
-        return self.id_to_multipolygon[id]
 
     def __xml__(self):
         inner_child_list = []
-        for id, multipolygon in self.id_to_multipolygon.items():
-            attribs = {}
-            attribs['fill'] = self.colored_view.get_color(id)
+        for id in self.ids:
+            multipolygon = self.get_id_to_multipolygon(id)
+            attribs = {'fill': self.get_id_to_color(id)}
 
             inner_child_list.append(
                 self.palette.draw_multipolygon(
@@ -66,15 +39,14 @@ class PolygonView(ABC):
                 )
             )
 
+        for id in self.ids:
+            multipolygon = self.get_id_to_multipolygon(id)
+            (cx, cy), (rx, ry) = xy.get_cxcyrxry(multipolygon)
+            inner_child_list.append(
+                self.get_id_to_label(id, (cx, cy), (rx, ry)),
+            )
+
         return self.palette.draw_g(
-            inner_child_list + [
-                self.colored_view.__xml__(),
-                self.labelled_view.__xml__(),
-            ])
-
-    # For LabelledView
-    def get_label_xy(self, id):
-        return xy.get_midxy(self.get_multipolygon(id))
-
-    def get_label_r(self, id):
-        return self.palette.get_radius(self.get_multipolygon(id))
+            inner_child_list,
+            self.children,
+        )
