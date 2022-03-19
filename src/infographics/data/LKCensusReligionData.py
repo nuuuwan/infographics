813591cccfe66213efd8f0d56e2214427af1838f
nuuuwan <nuuuwan@gmail.com>
2022-03-19 14:04:58 +0530

    Added get_color_value_to_color; Fixed #BUG in TABLE_ID

--- src/infographics/data/LKCensusReligionData.py
+++ src/infographics/data/LKCensusReligionData.py
@@ -4,7 +4,17 @@ from infographics.data.LKCensusData import LKCensusData
 
 
 class LKCensusReligionData(LKCensusData):
-    TABLE_ID = 'religions_affiliation_of_population'
+    TABLE_ID = 'religious_affiliation_of_population'
 
     def __init__(self):
         LKCensusData.__init__(self, LKCensusReligionData.TABLE_ID)
+
+    @staticmethod
+    def get_color_value_to_color(color_value):
+        return {
+            'buddhist': 'yellow',
+            'hindu': 'orange',
+            'islam': 'green',
+            'roman_catholic': 'blue',
+            'other_christian': 'purple',
+        }.get(color_value)
