3711d9d657d54b5fca8edfb061bc5e0cd6905bf7
nuuuwan <nuuuwan@gmail.com>
2022-03-19 21:42:51 +0530

    Split df_to_geodata_index into smaller functions

--- src/infographics/base/pandax.py
+++ src/infographics/base/pandax.py
@@ -3,31 +3,41 @@ from shapely.geometry import MultiPolygon, Polygon
 from infographics.base import shapely
 
 
+def shape_to_shapely_polygon_list(shape):
+    if isinstance(shape, MultiPolygon):
+        return shapely.shapely_multipolygon_to_polygon_list(shape)
+    elif isinstance(shape, Polygon):
+        return [shape]
+    else:
+        raise Exception('Unknown shape: ', type(shape))
+
+
+def shapely_polygon_list_to_multipolygon(shapely_polygon_list):
+    point_list_list = list(map(
+        shapely.shapely_polygon_to_point_list,
+        shapely_polygon_list,
+    ))
+
+    multipolygon = list(map(
+        lambda point_list: list(map(
+            shapely.shapely_point_to_xy,
+            point_list,
+        )),
+        point_list_list,
+    ))
+
+    return multipolygon
+
+
 def df_to_geodata_index(df):
     geodata_index = {}
     for row in df.itertuples():
         d = dict(row._asdict())
 
-        shape = d['geometry']
-        if isinstance(shape, MultiPolygon):
-            polygon_list = shapely.shapely_multipolygon_to_polygon_list(shape)
-        elif isinstance(shape, Polygon):
-            polygon_list = [shape]
-        else:
-            polygon_list = []
-
-        point_list_list = list(map(
-            shapely.shapely_polygon_to_point_list,
-            polygon_list,
-        ))
-
-        multipolygon = list(map(
-            lambda point_list: list(map(
-                shapely.shapely_point_to_xy,
-                point_list,
-            )),
-            point_list_list,
-        ))
+        shapely_polygon_list = shape_to_shapely_polygon_list(d['geometry'])
+        multipolygon = shapely_polygon_list_to_multipolygon(
+            shapely_polygon_list,
+        )
 
         del d['geometry']
         geodata_index[d['id']] = d | {
