e9b75f6f4270fb90c3e9c344f29d4dc88577afcb
nuuuwan <nuuuwan@gmail.com>
2022-03-19 12:00:15 +0530

    Refactored

--- new_examples/example3.py
+++ new_examples/example3.py
@@ -1,9 +1,7 @@
-from gig import ent_types, ents
 
-from infographics.base import xy
+from infographics.adaptors import ColorBase, SimpleLabel
 from infographics.core import Infographic
-from infographics.core.SVGPalette import SVGPalette
-from infographics.data import LKCensusData, LKGeoData
+from infographics.data import LKCensusData, LKGeoData, gig_utils
 from infographics.view import LegendView, PolygonView
 from new_examples.examples import example_svg_file_name
 
@@ -11,105 +9,64 @@ from new_examples.examples import example_svg_file_name
 def main():
     region_id = 'LK'
     subregion_type = 'dsd'
-    region_ent = ents.get_entity(region_id)
-    region_name = region_ent['name']
-    region_entity_type = ent_types.get_entity_type(region_id)
-    title = f'{region_name} {region_entity_type.upper()}'
-    subtitle = f'Most Common Ethnicity by {subregion_type.upper()}'
 
-    palette = SVGPalette()
-    lk_geodata = LKGeoData(
-        region_id=region_id,
-        subregion_type=subregion_type,
-    ).data
-    multi2polygon = list(map(lambda d: d['multipolygon'], lk_geodata.values()))
-    norm_multi2polygon = xy.get_norm_multi2polygon(multi2polygon)
-    for i, id in enumerate(list(lk_geodata.keys())):
-        lk_geodata[id]['norm_multipolygon'] = norm_multi2polygon[i]
+    lk_geodata = LKGeoData(region_id, subregion_type)
+    lk_census_data = LKCensusData('ethnicity_of_population')
 
-    def get_id_to_norm_multipolygon(id):
-        return lk_geodata[id]['norm_multipolygon']
+    MAJORITY_LIMIT = 0.55
 
-    lk_census_data = LKCensusData(
-        table_id='ethnicity_of_population',
-    ).data
-
-    MAJORITY_LIMIT = 0.6
-
-    color_value_to_color = {
-        'sinhalese': 'maroon',
-        'tamil': 'orange',
-        'muslim': 'green',
-        'none': 'white',
-    }
-
-    label_majority_limit = f'> {MAJORITY_LIMIT:.0%}'
-    color_value_to_label = {
-        'sinhalese': f'Sinhalese {label_majority_limit}',
-        'tamil': f'Tamil {label_majority_limit}',
-        'muslim': f'Muslim {label_majority_limit}',
-        'none': f'No ethnicity with {label_majority_limit}',
-    }
-
-    id_to_color_value = {}
-    for id in lk_geodata:
+    def id_to_most_common_ethnicity(id):
         d = lk_census_data[id]
         n_sinhala = d['sinhalese']
         n_tamil = d['sl_tamil'] + d['ind_tamil']
         n_muslim = d['sl_moor'] + d['malay']
         n_total = d['total_population']
 
-        max_color_value = 'none'
         if n_sinhala > n_total * MAJORITY_LIMIT:
-            max_color_value = 'sinhalese'
-        elif n_tamil > n_total * MAJORITY_LIMIT:
-            max_color_value = 'tamil'
-        elif n_muslim > n_total * MAJORITY_LIMIT:
-            max_color_value = 'muslim'
-
-        id_to_color_value[id] = max_color_value
-
-    def get_id_to_label(id, cxy, rxy):
-        label = lk_geodata[id]['name']
-        rx, ry = rxy
-        font_size = palette.actual_width * rx / len(label) / 16
-        return palette.draw_text(
-            label,
-            cxy,
-            font_size,
-        )
-
-    def get_id_to_color(id):
-        return color_value_to_color.get(
-            id_to_color_value[id],
-            'gray',
-        )
-
-    def get_color_value_to_label(color_value):
-        return color_value_to_label.get(color_value)
-
-    color_values = color_value_to_color.keys()
+            return 'sinhalese'
+        if n_tamil > n_total * MAJORITY_LIMIT:
+            return 'tamil'
+        if n_muslim > n_total * MAJORITY_LIMIT:
+            return 'muslim'
+        return 'none'
 
     def get_color_value_to_color(color_value):
-        return color_value_to_color[color_value]
+        return {
+            'sinhalese': 'maroon',
+            'tamil': 'orange',
+            'muslim': 'green',
+            'none': 'white',
+        }.get(color_value)
+
+    def get_color_value_to_label(color_value):
+        if color_value == 'none':
+            return f'No ethnicity with > {MAJORITY_LIMIT:.0%}'
+        return color_value.title()
+
+    color_base = ColorBase(
+        lk_geodata.keys(),
+        id_to_most_common_ethnicity,
+        get_color_value_to_color,
+    )
+    simple_label = SimpleLabel(lk_geodata.get_id_to_name)
 
     infographic = Infographic(
-        title=title,
-        subtitle=subtitle,
-        footer_text='visualization by @nuuuwan',
+        gig_utils.get_full_name(region_id),
+        gig_utils.get_by_name(subregion_type, 'Population Density'),
+        'visualization by @nuuuwan',
         children=[
             PolygonView(
-                ids=lk_geodata.keys(),
-                get_id_to_norm_multipolygon=get_id_to_norm_multipolygon,
-                get_id_to_color=get_id_to_color,
-                get_id_to_label=get_id_to_label,
-                children=[],
+                lk_geodata.keys(),
+                lk_geodata.get_id_to_norm_multipolygon,
+                color_base.get_id_to_color,
+                simple_label.get_id_to_label,
+                [],
             ),
             LegendView(
-                legend_title='Most Common Ethnicity',
-                color_values=color_values,
-                get_color_value_to_color=get_color_value_to_color,
-                get_color_value_to_label=get_color_value_to_label,
+                'Most Common Ethnicity',
+                color_base.unique_color_values,
+                get_color_value_to_color,
+                get_color_value_to_label,
             )
         ]
     )
