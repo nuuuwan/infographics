2abedc41a735ddcd47e8f40c8449f4f7c972d297
nuuuwan <nuuuwan@gmail.com>
2022-03-19 10:06:28 +0530

    Added get_norm_data; Changed data -> get_data;

--- src/infographics/data/LKGeoData.py
+++ src/infographics/data/LKGeoData.py
@@ -1,9 +1,9 @@
-from functools import cached_property
+from functools import cache
 
 from geo import geodata
 
 from infographics._utils import log
-from infographics.base import pandax
+from infographics.base import pandax, xy
 
 log.debug('[?expensive] from geo import geodata...')
 
@@ -17,8 +17,8 @@ class LKGeoData:
         self.region_id = region_id
         self.subregion_type = subregion_type
 
-    @cached_property
-    def data(self):
+    @cache
+    def get_data(self):
         log.debug('[expensive] geodata.get_region_geodata...')
         df = geodata.get_region_geodata(
             self.region_id,
@@ -26,3 +26,12 @@ class LKGeoData:
         )
         geodata_index = pandax.df_to_geodata_index(df)
         return geodata_index
+
+    @cache
+    def get_norm_data(self):
+        data = self.get_data()
+        multi2polygon = list(map(lambda d: d['multipolygon'], data.values()))
+        norm_multi2polygon = xy.get_norm_multi2polygon(multi2polygon)
+        for i, id in enumerate(list(data.keys())):
+            data[id]['norm_multipolygon'] = norm_multi2polygon[i]
+        return data
