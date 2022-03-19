79b92613e9660673c1099e30fa12323c663be7c9
nuuuwan <nuuuwan@gmail.com>
2022-03-19 16:42:24 +0530

    [main] Removed examples (2022-03-19-1642)

--- examples/example1.py
+++
@@ -1,8 +0,0 @@
-from infographics.Infographic import Infographic
-from infographics.LKMap import LKMap
-
-if __name__ == '__main__':
-    Infographic(
-        title='Sri Lanka',
-        children=[LKMap(figure_text='Sri Lanka')],
-    ).save('/tmp/infographics.example1.png')
