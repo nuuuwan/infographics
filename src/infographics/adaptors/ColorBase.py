a1d4a7a3395d6488f02ffe3f1189f85799203219
nuuuwan <nuuuwan@gmail.com>
2022-03-19 11:51:36 +0530

    Added ColorBase; and moved functions form ColorHistogram to ColorBase;

---
+++ src/infographics/adaptors/ColorBase.py
@@ -0,0 +1,38 @@
+from functools import cached_property, cache
+
+
+class ColorBase:
+    def __init__(
+        self,
+        ids,
+        get_id_to_color_value,
+        get_color_value_to_color,
+    ):
+        self.ids = ids
+        self.get_id_to_color_value = get_id_to_color_value
+        self.get_color_value_to_color = get_color_value_to_color
+
+    @cached_property
+    def sorted_color_values(self):
+        return sorted(list(map(
+            lambda id: self.get_id_to_color_value(id),
+            self.ids,
+        )))
+
+    @cache
+    def get_color_values(self, legend_size):
+        sorted_color_values = self.sorted_color_values
+        n = len(sorted_color_values)
+        legend_color_values = []
+        for i in range(0, legend_size):
+            j = (int)(i * (n - 1) / (legend_size - 1))
+            legend_color_values.append(sorted_color_values[j])
+        return legend_color_values
+
+    def get_id_to_color(self, id):
+        color_value = self.get_id_to_color_value(id)
+        return self.get_color_value_to_color(color_value)
+
+    def get_color_value_to_int_label(self, color_value):
+        color_value = (int)(round(color_value, 0))
+        return f'{color_value:,}'
