8dd3c601ad36459103ce29153b0419b28b4fc5e7
nuuuwan <nuuuwan@gmail.com>
2022-03-19 10:36:55 +0530

    Added keys, get_id_to_norm_multipolygon, __getitem__ and __len__

--- src/infographics/data/LKGeoData.py
+++ src/infographics/data/LKGeoData.py
@@ -28,6 +28,10 @@ class LKGeoData:
         return geodata_index
 
     @cache
+    def keys(self):
+        return self.get_data().keys()
+
+    @cache
     def get_norm_data(self):
         data = self.get_data()
         multi2polygon = list(map(lambda d: d['multipolygon'], data.values()))
@@ -35,3 +39,12 @@ class LKGeoData:
         for i, id in enumerate(list(data.keys())):
             data[id]['norm_multipolygon'] = norm_multi2polygon[i]
         return data
+
+    def get_id_to_norm_multipolygon(self, id):
+        return self[id]['norm_multipolygon']
+
+    def __getitem__(self, id):
+        return self.get_norm_data().get(id)
+
+    def __len__(self):
+        return len(self.get_norm_data())
