be54c82061d5c8dca7082d281b761ff4a5e38212
nuuuwan <nuuuwan@gmail.com>
2022-03-19 21:54:50 +0530

    Modified percent

--- src/infographics/base/Humanize.py
+++ src/infographics/base/Humanize.py
@@ -9,10 +9,9 @@ def number(f):
 
 
 def percent(p):
-    if p > 0.1:
-        return f'{p:.0%}'
-    if p > 0.01:
-        return f'{p:.1%}'
-    if p > 0.001:
-        return f'{p:.2%}'
+    for n_decimals in range(0, 3):
+        p_limit = 0.1 ** (1 + n_decimals)
+        if p > p_limit:
+            format_str = '{p:.%d%%}' % (n_decimals)
+            return format_str.format(p=p)
     return '<0.1%'
