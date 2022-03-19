bfdc25e01b222437d3eaeb629181e327ccd226bb
nuuuwan <nuuuwan@gmail.com>
2022-03-19 11:38:45 +0530

    Added keys, __getitem__ and __len__

--- src/infographics/data/LKCensusData.py
+++ src/infographics/data/LKCensusData.py
@@ -1,4 +1,4 @@
-from functools import cached_property
+from functools import cached_property, cache
 
 from gig import ext_data
 
@@ -19,3 +19,13 @@ class LKCensusData:
     def data(self):
         log.debug('[expensive] calling ext_data._get_table_index')
         return ext_data._get_table_index(DATA_GROUP, self.table_id)
+
+    @cache
+    def keys(self):
+        return self.get_data().keys()
+
+    def __getitem__(self, id):
+        return self.get_data().get(id)
+
+    def __len__(self):
+        return len(self.keys())
