938da6941cda738e49bde65b5cbdb497da2e4951
nuuuwan <nuuuwan@gmail.com>
2022-03-19 11:13:11 +0530

    Added gig_utils with get_region_full_name

---
+++ src/infographics/data/gig_utils.py
@@ -0,0 +1,10 @@
+from gig import ent_types, ents
+
+
+def get_region_full_name(region_id):
+    region_ent = ents.get_entity(region_id)
+    region_name = region_ent['name']
+    region_entity_type = ent_types.get_entity_type(region_id)
+    if region_entity_type == 'country':
+        return region_name
+    return f'{region_name} {region_entity_type.upper()}'
