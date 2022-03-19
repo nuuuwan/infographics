6be7c4c830ea87ee573c21184edd472ca74fd372
nuuuwan <nuuuwan@gmail.com>
2022-03-19 22:23:54 +0530

    Removed draw_multi2polygon, get_radius, get_relative_font_width; refactored;

--- src/infographics/core/SVGPalettePolygon.py
+++ src/infographics/core/SVGPalettePolygon.py
@@ -1,7 +1,6 @@
 
 from utils.xmlx import _
 
-from infographics.base import xy
 from infographics.core.SVG_STYLES import SVG_STYLES
 
 DEFAULT_WIDTH, DEFAULT_HEIGHT, PADDING = 1200, 675, 20
@@ -9,42 +8,21 @@ DEFAULT_BASE_FONT_SIZE = 16
 
 
 class SVGPalettePolygon:
-    def get_radius(self, multipolygon):
-        x_span, ___ = xy.get_spans(multipolygon)
-        return x_span / 2
-
-    def get_relative_font_width(self, multipolygon):
-        return self.actual_width * \
-            (self.get_radius(multipolygon) * 2) / DEFAULT_BASE_FONT_SIZE
-
-    def draw_multi2polygon(self, multi2polygon, child_list=[], attribs={}):
+    def draw_multipolygon(self, multipolygon, attribs={}):
         return self.draw_g(
             list(map(
-                lambda multipolygon: self.draw_multipolygon(
-                    multipolygon,
-                    [],
-                    attribs,
-                ),
-                multi2polygon,
-            )) + child_list,
-            attribs,
-        )
-
-    def draw_multipolygon(self, multipolygon, child_list=[], attribs={}):
-        return self.draw_g(
-            list(map(
-                lambda polygon: self.draw_polygon(polygon, [], attribs),
+                lambda polygon: self.draw_polygon(polygon, attribs),
                 multipolygon,
-            )) + child_list,
-            attribs,
+            )),
         )
 
-    def draw_polygon(self, polygon, child_list, attribs={}):
+    def draw_polygon(self, polygon, attribs={}):
         d_list = []
         for p in polygon:
             x, y = self.t(p)
-            prefix = 'M' if (not d_list) else 'L'
-            d_list.append('%s%d,%d' % (prefix, x, y))
+            prefix = 'L' if (d_list) else 'M'
+            d_list.append(f'{prefix}{x},{y}')
         d_list.append('z')
         d = ' '.join(d_list)
-        return _('path', child_list, SVG_STYLES.PATH | {'d': d} | attribs)
+
+        return _('path', [], SVG_STYLES.PATH | {'d': d} | attribs)
