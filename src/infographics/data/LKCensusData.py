f408dbb8198cc57579fb69952bbad8011cd6d3f7
nuuuwan <nuuuwan@gmail.com>
2022-03-19 13:12:51 +0530

    Added get_get_id_to_p_population,  get_id_to_total_population

--- src/infographics/data/LKCensusData.py
+++ src/infographics/data/LKCensusData.py
@@ -11,14 +11,14 @@ class LKCensusData:
     def __init__(self, table_id):
         self.table_id = table_id
 
-    @property
-    def total_field(self):
-        return 'total_population'
-
     @cache
     def get_data(self):
         log.debug('[expensive] calling ext_data._get_table_index')
         return ext_data._get_table_index(DATA_GROUP, self.table_id)
+
+    @cache
+    def get_id_to_total_population(self, id):
+        return self[id]['total_population']
 
     @cache
     def keys(self):
@@ -29,3 +29,11 @@ class LKCensusData:
 
     def __len__(self):
         return len(self.keys())
+
+    def get_get_id_to_p_population(self, field_list):
+        def get_id_to_p_population(id):
+            d = self[id]
+            n_total = self.get_id_to_total_population(id)
+            n_fields = sum([d[field] for field in field_list])
+            return n_fields / n_total
+        return get_id_to_p_population
