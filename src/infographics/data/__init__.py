56374aa095e76db34b40c317f58d3e58ddfe4954
nuuuwan <nuuuwan@gmail.com>
2022-03-20 19:32:40 +0530

    Moved GIGData logic to GIGData

--- src/infographics/data/__init__.py
+++ src/infographics/data/__init__.py
@@ -1,3 +1,4 @@
+from infographics.data.GIGData import GIGData
 from infographics.data.LKCensusData import LKCensusData
 from infographics.data.LKCensusEthnicityData import LKCensusEthnicityData
 from infographics.data.LKCensusReligionData import LKCensusReligionData
