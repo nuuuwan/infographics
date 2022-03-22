d4b373ac7eb3e2d6eebb78b72dbc134d5f663428
nuuuwan <nuuuwan@gmail.com>
2022-03-22 15:39:15 +0530

    Moved get_shapely_polygon_list_from_shape and get_multipolygon_from_shapely_polygon_list to shapely; Minor bug fixes;

--- src/infographics/base/pandax.py
+++ src/infographics/base/pandax.py
@@ -1,31 +1,4 @@
-from shapely.geometry import MultiPolygon, Polygon
-
 from infographics.base import ds, shapely
-
-
-def get_shapely_polygon_list_from_shape(shape):
-    if isinstance(shape, MultiPolygon):
-        return shapely.get_shapely_polygon_list_from_multipolygon(shape)
-    if isinstance(shape, Polygon):
-        return [shape]
-    raise Exception('Unknown shape: ', type(shape))
-
-
-def get_multipolygon_from_shapely_polygon_list(shapely_polygon_list):
-    point_list_list = list(map(
-        shapely.get_point_list_from_shapely_polygon_to,
-        shapely_polygon_list,
-    ))
-
-    multipolygon = list(map(
-        lambda point_list: list(map(
-            shapely.get_xy_from_shapely_point,
-            point_list,
-        )),
-        point_list_list,
-    ))
-
-    return multipolygon
 
 
 def get_geodata_index(df):
@@ -33,8 +6,8 @@ def get_geodata_index(df):
     for row in df.itertuples():
         d = dict(row._asdict())
 
-        shapely_polygon_list = get_shapely_polygon_list_from_shape(d['geometry'])
-        multipolygon = get_multipolygon_from_shapely_polygon_list(
+        shapely_polygon_list = shapely.get_shapely_polygon_list_from_shape(d['geometry'])
+        multipolygon = shapely.get_multipolygon_from_shapely_polygon_list(
             shapely_polygon_list,
         )
 
