0c9669706940a0a399ae2a07b3bf21e8591ca1ac
nuuuwan <nuuuwan@gmail.com>
2022-03-26 17:18:57 +0530

    Completed example11

--- examples/example11.py
+++ examples/example11.py
@@ -1,20 +1,23 @@
 from infographics.core import Infographic
 from infographics.data import LKElectionsData
+from infographics.view import BarGraphBasic
 
 if __name__ == '__main__':
     data = [
-        [1994, [[1, 'with SLPF']]],
+        [1994, [[1, 'JVP (SLPF)']]],
         [2000, [[10, 'JVP']]],
         [2001, [[16, 'JVP']]],
-        [2004, [[39, 'JVP with PA']]],
-        [2010, [[4, 'JVP with DNA'], [3, 'NFF with PA']]],
-        [2015, [[6, 'JVP'], [5, 'NFF with PA']]],
-        [2020, [[3, 'JVP with NPP'], [6, 'NFF with PA']]],
+        [2004, [[39, 'JVP (UPFA)']]],
+        [2010, [[4, 'JVP (DNA)'], [3, 'NFF (UPFA)']]],
+        [2015, [[6, 'JVP'], [5, 'NFF (UPFA)']]],
+        [2020, [[3, 'JVP (NPP)'], [6, 'NFF (SLPFA)']]],
     ]
     Infographic(
         'JVP and related parties',
-        'Seats in the Sri Lankan Parliament',
+        'Seats in the Sri Lankan Parliament (Since 1999)',
         LKElectionsData.SOURCE_TEXT,
         Infographic.DEFAULT_FOOTER_TEXT,
-        children=[]
+        children=[
+            BarGraphBasic(data),
+        ]
     ).save('/tmp/infographics.example11.svg')
