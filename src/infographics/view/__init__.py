d186994ffd0de8799cfe63ea186dbf4527cf142d
nuuuwan <nuuuwan@gmail.com>
2022-03-26 17:18:40 +0530

    Added BarGraphBasic

--- src/infographics/view/__init__.py
+++ src/infographics/view/__init__.py
@@ -1,3 +1,4 @@
+from infographics.view.BarGraphBasic import BarGraphBasic
 from infographics.view.DorlingView import DorlingView
 from infographics.view.FlagDorlingView import FlagDorlingView
 from infographics.view.LegendView import LegendView
